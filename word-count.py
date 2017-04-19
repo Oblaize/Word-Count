97fe9821c29fc734295e82641decc36e9c11f5df
Blaize Ottizy <Blaize Ottizy>
2017-04-19 13:53:14 +0300

    the word count code

---
+++ word-count.py
@@ -0,0 +1,21 @@
+def words(words_in):
+    """Counts the occurrences or characters in a word
+
+    """
+    # replace tabs and newline occurrences with spaces and split
+    # the string where spaces occur
+    words_list = words_in.replace('\n', ' ').replace('\t', ' ').split(' ')
+    # filter out spaces
+    words_list = [word for word in words_list if word]
+    # create a dictionary of words and the occurrence count using
+    # dictionary comprehension
+    words_count = {word: words_list.count(word) for word in set(words_list)}
+
+    # convert keys that are digits into integers
+    for key, val in words_count.items():
+        if isinstance(key, str) and key.isdigit():
+            words_count[int(key)] = val
+            words_count.pop(key)
+
+    # return dictionary
+    return words_count
